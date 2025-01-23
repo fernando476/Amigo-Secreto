e6685b3b89e54539bd60ac70979b0bd41d62294e
Fernando Henrique <fernandohenriquecaragua@gmail.com>
2025-01-23 14:11:33 -0300

    amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,73 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let numerosGerados = [];
+
+function adicionarAmigo(){
+    const nomeAmigo = document.getElementById('amigo').value.trim();
+
+    if (validarNomeAmigo(nomeAmigo)) {
+        atualizarArrayAmigo(nomeAmigo);
+        atualizarListaExibicao();
+        limparInput();
+    }
+}
+
+function validarNomeAmigo(nome) {
+    if (nome === '') {
+        alert('Por favor, insira um nome.');
+        return false;
+    }
+    return true;
+}
+
+function atualizarArrayAmigo(nome) {
+    amigos.push(nome);
+}
+
+function limparInput() {
+    document.getElementById('amigo').value = '';
+}
+
+function atualizarListaExibicao() {
+    const listaExibicao = document.getElementById('listaAmigos');
+    listaExibicao.innerHTML = '';
+
+    amigos.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        listaExibicao.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Por favor, adicione amigos antes de sortear.');
+        return;
+    }
+
+    const numero = gerarNumeroAleatorio();
+    if (numero !== 'limite') {
+        mostrarSorteado(numero);
+    }
+}
+
+function gerarNumeroAleatorio() {
+    const tamanhoArray = amigos.length;
+    const novoNumeroGerado = Math.floor(Math.random() * tamanhoArray);
+
+    if (tamanhoArray === numerosGerados.length) {
+        alert('Todos os nomes foram sorteados. Por favor reinicie o jogo ou adicione mais amigos.');
+        return 'limite';
+    }
+
+    if (numerosGerados.includes(novoNumeroGerado)) {
+        return gerarNumeroAleatorio();
+    } else {
+        numerosGerados.push(novoNumeroGerado);
+        return novoNumeroGerado;
+    }
+}
+
+function mostrarSorteado(numero) {
+    const sorteado = document.getElementById('resultado');
+    sorteado.innerHTML = `<li>${amigos[numero]}</li>`;
+}
